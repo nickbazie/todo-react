e2fbbb67ca8c75de85df2ca68943f009bb869e35
Nico <nickbazie@gmail.com>
2020-07-19 18:45:17 +0200

    Updates

---
+++ precache-manifest.0236cb5763239831e58d70cd32ddddd6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "801f16f53c681776d29406cf86a86320",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "30f7ca68147c7489002b",
+    "url": "/todo-react/static/js/2.be571024.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-react/static/js/2.be571024.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfc9b4f0d061a5f512d1",
+    "url": "/todo-react/static/js/main.a993375f.chunk.js"
+  },
+  {
+    "revision": "a049eaa3d15bfcc71b2e",
+    "url": "/todo-react/static/js/runtime-main.b204df10.js"
+  }
+]);
