0a7e0e185463ad85713ab9dcdb6d31ce833852a2
Nico <nickbazie@gmail.com>
2020-07-25 17:34:57 +0200

    Added more CSS, buttons and searchbar

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -1,13 +1,11 @@
 import React from 'react';
 
 import TodoListItem from './todo-list-item';
-
 import './todo-list.css';
 
 const TodoList = ({ todos }) => {
 
     const elements = todos.map((item) => {
-
         const { id, ...itemProps } = item;
 
         return (
--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import ReactDOM from 'react-dom';
 import AppHeader from './components/app-header';
 import SearchPanel from './components/search-panel';
 import TodoList from './components/todo-list';
+import ItemStatusFilter from './components/item-status-filter';
 
+import './index.css';
 
 const App = () => {
 
@@ -13,10 +15,15 @@ const App = () => {
         { label: 'Make Awesome App', important: true, id: 2 },
         { label: 'Have lunch', important: false, id: 3 }
     ];
+    
     return (    
-        <div>
-            <AppHeader />
-            <SearchPanel />
+        <div className="todo-app">
+            <AppHeader toDo={1} done={3} />
+            <div className="top-panel d-flex">
+                <SearchPanel />
+                <ItemStatusFilter />
+            </div>
+
             <TodoList todos={todoData} />
         </div>
     );
